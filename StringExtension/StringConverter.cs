146301e3d13a3e864de0a1b306e7c702867cab86
Arina <arinkarus@users.noreply.github.com>
2019-04-29 12:05:57 +0300

    Update StringConverter.cs

--- StringExtension/StringConverter.cs
+++ StringExtension/StringConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace StringExtension
 {
@@ -6,7 +7,39 @@ namespace StringExtension
     {
         public string Convert(string source, int count)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (source == string.Empty)
+            {
+                throw new ArgumentException();
+            }
+            if (source.Trim().Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            if (count < 0)
+            {
+                throw new ArgumentException();
+            }
+            var stringBuilder = new StringBuilder(source);
+
+            for (int j = 0; j < count; j++)
+            {
+                  for (int i = 0; i < source.Length; i = i + 2)
+                  {
+                      stringBuilder.Append(source[i]);
+                  }
+                  for (int i = 1; i < source.Length; i = i + 2)
+                  {
+                      stringBuilder.Append(source[i]);
+                  }
+
+                  source = stringBuilder.ToString();
+                  stringBuilder.Clear();
+            }
+            return stringBuilder.ToString();
         }
     }
 }
