a53197311597b845bacccb609f4deaf37e117e99
arinkarus <arinkarus@gmail.com>
2019-04-29 14:35:44 +0300

    Update StringConverter.cs

--- StringExtension/StringConverter.cs
+++ StringExtension/StringConverter.cs
@@ -7,40 +7,50 @@ namespace StringExtension
     {
         public string Convert(string source, int count)
         {
+            ValidateParams(source, count);
+            var stringBuilder = new StringBuilder(source);
+            var oddNumbers = new StringBuilder();
+            var evenNumbers = new StringBuilder();
+            int period = count % source.Length;
+            int iterationCountForSource = source.Length / 2;
+            for (int j = 0; j < period; j++)
+            {               
+                for (int i = 0; i < iterationCountForSource; i++)
+                {
+                    evenNumbers.Append(stringBuilder[2 * i]);
+                    oddNumbers.Append(stringBuilder[2 * i + 1]);
+                }
+
+                stringBuilder.Clear();
+                stringBuilder.Append(evenNumbers).Append(oddNumbers);
+                oddNumbers.Clear();
+                evenNumbers.Clear();
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private void ValidateParams(string source, int count)
+        {
             if (source == null)
             {
                 throw new ArgumentNullException();
             }
+
             if (source == string.Empty)
             {
                 throw new ArgumentException();
             }
+
             if (source.Trim().Length == 0)
             {
                 throw new ArgumentException();
             }
+
             if (count < 0)
             {
-                throw new ArgumentException();
+                throw new ArgumentOutOfRangeException();
             }
-            var stringBuilder = new StringBuilder();
-
-            for (int j = 0; j < count; j++)
-            {
-                  for (int i = 0; i < source.Length; i = i + 2)
-                  {
-                      stringBuilder.Append(source[i]);
-                  }
-                  for (int i = 1; i < source.Length; i = i + 2)
-                  {
-                      stringBuilder.Append(source[i]);
-                  }
-
-                  source = stringBuilder.ToString();
-                  stringBuilder.Clear();
-            }
-
-            return source;
         }
     }
 }
